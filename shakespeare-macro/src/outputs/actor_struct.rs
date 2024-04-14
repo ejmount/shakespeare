9a001c78efeeaa75b626f607f2011bf6d8a3c00d
Ewan Mount <ejmount@users.noreply.github.com>
2024-04-14 15:59:32 +0100

    Refactoring

--- shakespeare-macro/src/outputs/actor_struct.rs
+++ shakespeare-macro/src/outputs/actor_struct.rs
@@ -1,6 +1,7 @@
+use itertools::Itertools;
 use quote::{quote, ToTokens};
 use syn::parse::Parser;
-use syn::{Field, ImplItemFn, ItemImpl, ItemStruct, Path, Result, Visibility};
+use syn::{Field, ImplItemFn, ItemFn, ItemImpl, ItemStruct, Path, Result, Visibility};
 
 use crate::data::RoleName;
 use crate::declarations::actor::ActorDecl;
@@ -9,9 +10,9 @@ use crate::macros::{fallible_quote, map_or_bail};
 
 #[derive(Debug)]
 pub struct ActorStruct {
-	strukt:     ItemStruct,
-	accesors:   ItemImpl,
-	meta_trait: ItemImpl,
+	strukt:        ItemStruct,
+	accessor_impl: ItemImpl,
+	meta_trait:    ItemImpl,
 }
 
 impl ActorStruct {
@@ -27,59 +28,25 @@ impl ActorStruct {
 
 		let fields = map_or_bail!(performances, make_field_from_name);
 
-		let accessors = map_or_bail!(performances, |perf| make_accessor_from_name(
-			perf.get_role_name(),
-			actor_vis
-		));
-
-		let getters = map_or_bail!(performances, |perf| make_sender_getter_from_name(
-			perf.get_role_name(),
-			actor_vis
-		));
-
 		let strukt = fallible_quote! {
+			#[derive(Clone)]
 			#actor_vis struct #actor_name {
 				#(#fields),*
 			}
 		}?;
 
-		let accessor_impl = fallible_quote! {
-			impl #actor_name {
-				#(#accessors)*
-				#(#getters)*
-			}
-		}?;
+		let role_names = performances
+			.iter()
+			.map(PerformanceDecl::get_role_name)
+			.collect_vec();
 
-		let unit_type = fallible_quote!(()).unwrap();
-		let panic_type =
-			fallible_quote!(std::boxed::Box<dyn std::any::Any + std::marker::Send>).unwrap();
+		let accessor_impl = create_inherent_impl(&role_names, actor_vis, actor_name)?;
 
-		let panic_return = panic_handler.as_ref().map_or(&panic_type, |f| {
-			if let syn::ReturnType::Type(_, ref b) = f.sig.output {
-				&**b
-			} else {
-				&unit_type
-			}
-		});
-
-		let exit_return = exit_handler.as_ref().map_or(&unit_type, |f| {
-			if let syn::ReturnType::Type(_, ref b) = f.sig.output {
-				&**b
-			} else {
-				&unit_type
-			}
-		});
-
-		let meta_trait = fallible_quote! {
-			impl ::shakespeare::ActorShell for #actor_name {
-				type ExitType = #exit_return;
-				type PanicType = #panic_return;
-			}
-		}?;
+		let meta_trait = create_meta_trait_impl(panic_handler, exit_handler, actor_name)?;
 
 		Ok(ActorStruct {
-			accesors: accessor_impl,
 			strukt,
+			accessor_impl,
 			meta_trait,
 		})
 	}
@@ -89,7 +56,78 @@ impl ToTokens for ActorStruct {
 	fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
 		self.strukt.to_tokens(tokens);
 		self.meta_trait.to_tokens(tokens);
-		self.accesors.to_tokens(tokens);
+		self.accessor_impl.to_tokens(tokens);
+	}
+}
+
+fn create_meta_trait_impl(
+	panic_handler: &Option<ItemFn>,
+	exit_handler: &Option<ItemFn>,
+	actor_name: &Path,
+) -> Result<ItemImpl> {
+	let unit_type = fallible_quote!(()).unwrap();
+	let panic_type =
+		fallible_quote!(std::boxed::Box<dyn std::any::Any + std::marker::Send>).unwrap();
+	let panic_return = panic_handler.as_ref().map_or(&panic_type, |f| {
+		if let syn::ReturnType::Type(_, ref b) = f.sig.output {
+			&**b
+		} else {
+			&unit_type
+		}
+	});
+	let exit_return = exit_handler.as_ref().map_or(&unit_type, |f| {
+		if let syn::ReturnType::Type(_, ref b) = f.sig.output {
+			&**b
+		} else {
+			&unit_type
+		}
+	});
+	fallible_quote! {
+		impl ::shakespeare::ActorShell for #actor_name {
+			type ExitType = #exit_return;
+			type PanicType = #panic_return;
+		}
+	}
+}
+
+fn create_inherent_impl(
+	role_names: &Vec<&RoleName>,
+	actor_vis: &Visibility,
+	actor_name: &Path,
+) -> Result<ItemImpl> {
+	fn accessor_from_name(role_name: &RoleName, vis: &Visibility) -> Result<ImplItemFn> {
+		let error_path: Path = fallible_quote! { shakespeare::Role2SendError<dyn #role_name> }?;
+		let payload_path = role_name.payload_path();
+		let field_name = role_name.queue_name();
+
+		let acccessor_name = role_name.acccessor_name();
+
+		fallible_quote! {
+			#vis async fn #acccessor_name(&self, payload: #payload_path) -> Result<(), #error_path>
+			{
+				self.#field_name.send(payload)
+			}
+		}
+	}
+	fn sender_getter_from_name(role_name: &RoleName, vis: &Visibility) -> Result<ImplItemFn> {
+		let field_name = role_name.queue_name();
+		let getter_name = role_name.sender_getter_name();
+
+		fallible_quote! {
+			#vis fn #getter_name(&self) -> &::shakespeare::Role2Sender<dyn #role_name>
+			{
+				&self.#field_name
+			}
+		}
+	}
+
+	let accessors = map_or_bail!(&role_names, |name| accessor_from_name(name, actor_vis));
+	let getters = map_or_bail!(&role_names, |name| sender_getter_from_name(name, actor_vis));
+	fallible_quote! {
+		impl #actor_name {
+			#(#accessors)*
+			#(#getters)*
+		}
 	}
 }
 
@@ -104,33 +142,4 @@ fn make_field_from_name(perf: &PerformanceDecl) -> Result<Field> {
 				format!("Parse failure trying to create actor field: {err} - this is a bug, please file an issue")
 			)
 		})
-}
-
-fn make_accessor_from_name(role_name: &RoleName, vis: &Visibility) -> Result<ImplItemFn> {
-	let payload_path = role_name.payload_path();
-	let field_name = role_name.queue_name();
-
-	let error_path: Path = fallible_quote! { shakespeare::Role2SendError<dyn #role_name> }?;
-
-	let acccessor_name = role_name.acccessor_name();
-
-	fallible_quote! {
-		#vis async fn #acccessor_name(&self, payload: #payload_path) -> Result<(), #error_path>
-		{
-			self.#field_name.send(payload)
-		}
-	}
-}
-
-fn make_sender_getter_from_name(role_name: &RoleName, vis: &Visibility) -> Result<ImplItemFn> {
-	let field_name = role_name.queue_name();
-
-	let getter_name = role_name.sender_getter_name();
-
-	fallible_quote! {
-		#vis fn #getter_name(&self) -> &::shakespeare::Role2Sender<dyn #role_name>
-		{
-			&self.#field_name
-		}
-	}
 }
