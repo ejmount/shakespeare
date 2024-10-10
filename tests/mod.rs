4a0e984429bf63f3a3abf07ac3bd84dde481cd4f
Ewan Mount <ejmount@users.noreply.github.com>
2024-10-10 23:47:51 +0100

    General workflow corrections

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,11 +1,13 @@
 #[test]
 fn ux() {
-	let t = trybuild::TestCases::new();
-	//t.pass("tests/successes/basic.rs");
-	//t.pass("tests/successes/modules.rs");
-	t.compile_fail("tests/fails/empty.rs");
-	t.compile_fail("tests/fails/missing_data.rs");
-	t.compile_fail("tests/fails/multiple_data.rs");
+	if option_env!("TOOLCHAIN").map_or(false, |s| s.contains("stable")) {
+		let t = trybuild::TestCases::new();
+		//t.pass("tests/successes/basic.rs");
+		//t.pass("tests/successes/modules.rs");
+		t.compile_fail("tests/fails/empty.rs");
+		t.compile_fail("tests/fails/missing_data.rs");
+		t.compile_fail("tests/fails/multiple_data.rs");
+	}
 }
 
 mod successes;
