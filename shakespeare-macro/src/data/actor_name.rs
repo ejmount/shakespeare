db109de4ac641519f218da18ac2fb05681fbacb0
Ewan Mount <ejmount@users.noreply.github.com>
2024-10-27 23:37:02 +0000

    Removing dead code

--- shakespeare-macro/src/data/actor_name.rs
+++ shakespeare-macro/src/data/actor_name.rs
@@ -1,17 +1,11 @@
-use convert_case::{Case, Casing};
-use quote::{format_ident, ToTokens};
-use syn::{Ident, TypePath};
+use quote::ToTokens;
+use syn::TypePath;
 
 pub(crate) struct ActorName(TypePath);
 
 impl ActorName {
 	pub(crate) fn new(p: TypePath) -> ActorName {
 		ActorName(p)
-	}
-
-	pub(crate) fn get_static_item_name(&self) -> Ident {
-		let ident = format!("{}Ref", self.0.path.segments.last().unwrap().ident);
-		format_ident!("{}", ident.to_case(Case::ScreamingSnake))
 	}
 }
 
