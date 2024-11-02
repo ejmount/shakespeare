20d771f6c200e0b947a5d4cb475dc31dd58f8996
Ewan Mount <ejmount@users.noreply.github.com>
2024-11-02 21:46:05 +0000

    Fn rename

--- shakespeare-macro/src/data/signature_ext.rs
+++ shakespeare-macro/src/data/signature_ext.rs
@@ -5,12 +5,12 @@ use syn::punctuated::Punctuated;
 use syn::{FnArg, PatType, Signature, Type};
 
 pub(crate) trait SignatureExt {
-	fn needs_context(&self) -> bool;
+	fn has_context_input(&self) -> bool;
 	fn remove_context_param(&mut self);
 }
 
 impl SignatureExt for Signature {
-	fn needs_context(&self) -> bool {
+	fn has_context_input(&self) -> bool {
 		if let Some(FnArg::Typed(PatType { ty, .. })) = self.inputs.iter().nth(1) {
 			if let Type::Reference(r) = &**ty {
 				r.lifetime.as_ref().map_or(false, |l| l.ident != "static")
@@ -23,7 +23,7 @@ impl SignatureExt for Signature {
 	}
 
 	fn remove_context_param(&mut self) {
-		if self.needs_context() {
+		if self.has_context_input() {
 			let mut items = replace(&mut self.inputs, Punctuated::new())
 				.into_iter()
 				.collect_vec();
