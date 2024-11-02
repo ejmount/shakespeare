43685d90b2e4478fb98b3536611714b39f3b0e52
Ewan Mount <ejmount@users.noreply.github.com>
2024-11-02 21:58:20 +0000

    Refactoring hook functions

--- shakespeare-macro/src/data/mod.rs
+++ shakespeare-macro/src/data/mod.rs
@@ -1,6 +1,7 @@
 mod actor_name;
 mod data_item;
 mod data_name;
+mod handler_functions;
 mod role_name;
 mod signature_ext;
 
@@ -14,4 +15,5 @@ pub(crate) type FunctionItem = syn::ImplItemFn;
 pub(crate) type MethodName = proc_macro2::Ident;
 pub(crate) type PayloadPath = syn::Path;
 
+pub(crate) use handler_functions::{FuncReturnType, HandlerFunctions};
 pub(crate) use signature_ext::SignatureExt;
