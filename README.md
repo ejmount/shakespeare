0ac284fe2d12e675e0d7de090863c61a61a352d6
Ewan Mount <ejmount@users.noreply.github.com>
2025-09-28 21:32:32 +0100

    Module-level documentation (and timeout tweak to match)

--- README.md
+++ README.md
@@ -8,23 +8,47 @@
 [![codecov](https://codecov.io/gh/ejmount/shakespeare/branch/main/graph/badge.svg?token=2L6ZS8OK32)](https://codecov.io/gh/ejmount/shakespeare)
 ![GitHub commits since latest release](https://img.shields.io/github/commits-since/ejmount/shakespeare/latest)
 
-Shakespeare is a framework written in Rust that focuses on ergonomics and extensibility in its implementation of the [actor model](https://en.wikipedia.org/wiki/Actor_model) for creating highly parallel yet safe and robust systems.
+Shakespeare is a pure Rust framework that focuses on ergonomics and extensibility in its implementation of the [actor model](https://en.wikipedia.org/wiki/Actor_model) for creating highly parallel yet safe and robust systems. Actors are run as tasks directly on top of [tokio](https://tokio.rs/) (with no "world" or "system" boundary) and run fully in parallel.
 
-Its most significant features include:
+**Why do I want an actor system?** - The actor model has many of the same architectural benefits as micro-services bring to complex systems, but on a smaller scale and with less overhead. (Namely, that no serialization is needed for actors on the same host.)
 
-* __Polymorphic actors__ - actors' interfaces are a first-class consideration in Shakespeare, and allowing code to work over dynamically chosen actors is a primary use case.
-* __Rich interfaces__ - a single interface for an actor can support any number of methods with no more boilerplate than the equivalent trait definition. This includes methods returning values, while the choice of whether or not to wait for the response remains with the caller and can be made on a call-by-call basis.
-* __Static validation__ - Shakespeare validates as much as possible at compile time, either by using macros, or expressing functions' preconditions through the type system. The macros similarly aim for statically-typed output, minimizing runtime overhead and offering more visibility to the optimizer.
-* __Interoperability__ - linking actors into the wider ecosystem of async code is designed to be easy - actor messages implement `Future`, and actors can interact with  `Future` and `Stream` generically.
-* __Recovery__ - an actor shutting down both runs a cleanup function within the actor and sends out a value indicating whether the shutdown was graceful (i.e. all references were dropped) or was a result of a panic within a message handler, along with any return value from the cleanup. Another actor can then subscribe to receive this value like any other `Future`.
+It is ideal for problems that decompose into separate tasks ("actors") that can be executed asyncronously, but where each task has state associated with it and access to that state must be coordinated. This coordination is then achieved by the actors passing messages to each other. Examples of this situation include messaging protocols like IRC and Matrix, distributed services like Kubernetes and Docker Swarm (and anything else using Raft consensus), and even some styles of video games. More specifically, the actor model means:
 
-Shakespeare currently runs exclusively on [tokio](https://tokio.rs/) but this may change in the future. It also currently uses only unbounded channels and so has no handling of backpressure, but improving this is planned future work.
+* Concurrency and scheduling policy fade into the background to leave the application logic more prominent, which can then be designed as it would be on a single-threaded system - any particular operation never needs to take locks on values it doesn't already own.
+* Actors' interfaces - and so allowed operations on the data - are declared explicitly rather than allowing a holder of a lock to make arbitrary changes to the shared value.
+  * Actors being the unique owners of their data also avoids all issues that would necessitate a lock hierachy.
+* Parallel execution falls out "for free" once the data model is designed
+
+**Why do I want *Shakespeare*?** - Shakespeare focuses on generality that allows the progarmmer to express their intent while preserving runtime performance, even if this may leave it with a higher barrier to entry than its alternatives. Its most significant features include:
+
+* **Polymorphic actors** - actors' interfaces ("roles") are a first-class consideration in Shakespeare, and ergonomically allowing an actor to have multiple roles, and a role to have multiple implementations, were important considerations. Allowing code to work with dynamically chosen actors of a given role is a primary use case, enabling not only polymorphism within application code but also allowing mocking in integration testing and the like.
+  * While this is achievable using a `Arc<Mutex<Queue>>` containing an enum, Shakespeare generates all of the glue to do so automatically.
+  * If a role is exported publicly, downstream crates can implement it on their own actors exactly as with normal traits.
+* **Rich interfaces** - a single role for an actor can support any number of messages and accompanying methods with no more boilerplate than the equivalent trait definition. Messages can include any number of input parameters of any type. This includes methods that return values, with the choice of whether or not to wait for the response remaining with the caller and being made on a call-by-call basis.
+  * There is [special support](https://docs.rs/shakespeare/latest/shakespeare/fn.send_reply_to.html) for relaying the returned value onwards without waiting for the remote processing to complete.
+* **Static validation** - Shakespeare attempts to validate as much as possible at compile time - actors are strongly typed throughought, and the library emphasizes expressing functions' preconditions through the type system. The output of Shakespeare's macros is similarly statically-typed throughout, minimizing runtime overhead and offering more visibility to the optimizer. Sending a message to and then awaiting the response from an actor of statically known type incurs *no* virtual function calls.
+* **Interoperability** - linking actors into the wider ecosystem of async code is designed to be easy - actor messages implement `Future`, and actors can treat [`Future`](https://docs.rs/shakespeare/latest/shakespeare/fn.send_future_to.html) and `Stream` objects as incoming messages with no indirection.
+  * Conversely, message handlers within an actor can be `async` and can await arbitrary Futures, with actor messages simply being a special case.
+* **Recovery** - Shakespeare makes it easy to respond an actor stopping its processing, both by providing a cleanup hooks within the actor and providing a `Future` to the caller (that originally created the actor) that will become ready when the spawned actor stops.
+  * Both the cleanup handling and the value from the cleanup `Future` indicate whether the shutdown was graceful (i.e. all references to the actor were dropped) or was a result of a panic within a message handler, so that error handling is more easily separated from the "happy path."
+  * Recovery handling is entirely optional - the cleanup `Future` can simply be dropped with no ill effect.  (Note in this case, the actor panicking will *not* propagate up)
+
+For a full explanation of how all this works, see [the crate documentation](https://docs.rs/shakespeare/latest/).
+
+It is worth noting that Shakespeare currently does **not** offer any built-in support for:
+
+* Global/static "broadcasts" of any kind - actors only have access to their own state and any static values your application code has defined.
+* Networking - while actor messages conceptually have the same semantics as remote procedure calls, Shakespeare currently only supports messages within a single host process.
+
+However, these capabilities are expected to be relatively straightforward to build in application code - one of the original imagined use cases involved "proxy" actors that implement a Role by forwarding the received messages to an actor on a remote host. If covering these use cases is important to you, please raise an issue and leave your feedback.
+
+Additionally, Shakespeare currently runs exclusively on tokio but this may change in the future. It also currently uses only unbounded channels, but improving this is planned future work.
 
 ## Example
 
 ```rust
 use std::sync::Arc;
-use shakespeare::{actor, performance, role, ActorSpawn};
+use shakespeare::{actor, performance, role};
 use tokio::sync::mpsc;
 
 #[role]
@@ -34,10 +58,11 @@ trait BasicRole {
 }
 
 #[actor]
-mod Actor {
+mod AnActor {
     struct StateA(mpsc::Sender<usize>);
+
     #[performance]
-    impl BasicRole for State {
+    impl BasicRole for StateA {
         async fn inform(&mut self, val: usize) {
             self.0.send(val).await;
         }
@@ -52,15 +77,17 @@ mod Actor {
 async fn main() {
     let (tx, mut rx) = mpsc::channel(1);
 
-    let actor: Arc<dyn BasicRole> = Actor::start(StateA(tx)).msg_handle;
-    {
-        actor.inform(100);
-        // can let the message drop to fire and forget
-    }
-    let value = rx.recv().await;
-    assert_eq!(value, Some(100));
+    let actor_state = StateA(tx);
+    let actor: Arc<dyn BasicRole> = AnActor::start(actor_state).actor_handle; // Actors can be upcast to a Role trait object
 
-    let ret_value = actor.get().await; // But also await to get a syncronous return value
+    {
+        let _ = actor.inform(100);
+        // can let the value drop to fire and forget, ignoring any return value from the actor
+    }
+    let chan_response = rx.recv().await;
+    assert_eq!(chan_response, Some(100));
+
+    let ret_value = actor.get().await; // But can also await to get a syncronous, strongly-typed return value
 
     assert_eq!(ret_value, Ok(42));
 }
@@ -68,7 +95,7 @@ async fn main() {
 
 ## Compatibility
 
-If you directly browse this crate's source code, you will come across functions marked `pub` but also `#[doc(hidden)]`. This is because the macros used in Shakespeare generate output code that then calls into these library functions. These calls technically originate from the *user's* crate, which means the called functions need to be `pub` to resolve. However, they are nonetheless not intended for direct client use, so they are suppressed from the documentation and __are exempt from SemVer__ - code bypassing documented interfaces may break even in patch releases.
+If you directly browse this crate's source code, you will come across functions marked `pub` but also `#[doc(hidden)]`. This is because the macros used in Shakespeare generate output code that then calls into these library functions. These calls technically originate from the *user's* crate, which means the called functions need to be `pub` to resolve. However, they are nonetheless not intended for direct client use, so they are suppressed from the documentation and **are exempt from SemVer** - code calling these functions by bypassing documented interfaces may break even in patch releases.
 
 ## Licence
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,130 @@
-//! Hello
-
+//! [![Github link](https://img.shields.io/badge/github-ejmount%2Fshakespeare-blue)](https://github.com/ejmount/shakespeare)
+//! [![Crates.io Version](https://img.shields.io/crates/v/shakespeare)](https://crates.io/crates/shakespeare)
+//!
+//! Shakespeare is an [actor system](https://en.wikipedia.org/wiki/Actor_model) focused on rich interfaces and polymorphism without impacting runtime performance.
+//!
+//! Shakespeare is based around three concepts:
+//! * [Actors](`shakespeare_macro::actor`) - objects that can asynchronously receive messages and run user-defined code in response. Actors contain state of an arbitrary type;  their response to a message will most commonly include updating their state, sending new messages and/or spawning additional actors but can in principle be executing arbitrary code.
+//!    * Some parts of this documentation refer specifically to the "actor shell", which is the automatically generated proxy object that handles the message passing into and out of the code you have written defining the actor's methods and state.
+//!    * There is no "world" or "system" boundary in Shakespeare - any part of your program can call methods on any actor for which it has a handle.
+//! * [Roles](`shakespeare_macro::role`) - traits for actors. A Role defines a set of methods that can be asynchronously called on an actor and which may be asynchronous themselves. These methods return a [`Envelope`] which is a `Future` of a return value, if any - the caller can use the envelope object to decide whether (and how) to await for any return value that the method may produce.
+//! * [Performances](`shakespeare_macro::performance`) - the block of code that actually defines how a given actor implements a role. This is in most ways a normal trait implementation, with the macro generating all of the glue code needed to call the defined methods as part of the actor's message loop.
+//!
+//! ## Using actors
+//!
+//! With a tokio runtime running, (e.g. via `#[tokio::main]`) an actor instance is created by calling the `start` function on the actor's shell type, which consumes a value of the corresponding state type. The `start` function is currently defined as having the same visibility as the defining actor module, i.e. defined as though with a bare `fn` keyword - if you want it to expose it more widely (including outside of the crate, i.e. `pub`) you will have to define a wrapper function for now. This requirement may be relaxed in a future version.
+//!
+//! The `start` function returns a [`ActorSpawn`], which contains two values:
+//! * an [`ActorHandle`] - this is a `Future` that yields when the actor exits, the value of which indicates whether the actor exited gracefully (all handles were dropped or a handler explicitly shut it down) or by one of the message handlers panicking. Dropping this value doesn't affect the actor's execution
+//! * an `Arc` containing the shell type - the first handle to the new actor. If all `Arcs` of the actor drop, the actor begins shutting down.
+//!
+//! At this point, the actor is active as a separate task, and will process any messages sent to its mailbox by calling the appropriate method from the corresponding performance definition.
+//!
+//! To send the actor a message, methods defined by the actor's roles can be called on the shell as normal, except that where the role defines a method with signature `... -> T` the call on the shell type instead returns [`Envelope<T>`]. For full details, see that type's documentation, but in most cases, you will want to do one of two things with it:
+//! * `await` it - an `Envelope` implements `IntoFuture` and will yield a value of `Ok(T)` assuming there are no problems with data transfer to the actor.
+//! * allow it to drop, which will dispatch the message to the destination actor's mailbox but not wait for any return value.
+//!
+//! Actors can also receive general [`Future`] and [`Stream`] values to their mailboxes, using [`send_future_to`] and [`send_stream_to`]. The types these functions can work on are specified by what implementations of the [`Accepts`] trait the actor has, see that trait documentation for details.
+//!
+//! **Note**: The API is designed to allow code to work with dynamically typed actors of a given role by using values of type `Arc<dyn Role>`, which `Arc<A>` can be upcast to by normal language rules. This construction does mean that the compiler may need help to correctly disambiguate [`send_future_to`] (and similar) calls.
+//!
+//!
+//! ## Defining an actor
+//!
+//! The starting point for defining a new actor is an `#[actor]` module block. The name of the actor shell type (from which `start` is called) is defined by the name of the module. As an example, to start the following, one would call `MyActor::start(SomeState)`:
+//!
+//! ```ignore
+//! #[actor]
+//! mod MyActor {
+//! 	enum SomeState {
+//! 		Some(Data),
+//! 		Empty,
+//! 	}
+//! 	// ...
+//! }
+//! ```
+//!
+//! The module needs to contain exactly one `struct`, `enum` *or* `union`, which serves as the state type of the actor. This type must be `'static`, `Sized`, and cannot contain free generic parameters (`enum SomeState<T>` would not be allowed) but there are no other restrictions on its content. It does not move after the actor is started, so storing large amounts of data inline is not a performance concern.
+//!
+//! ### Performances
+//!
+//! The module must also contain at least one `#[performance]` trait impl block:
+//!
+//! ```ignore
+//! #[performance]
+//! impl ARole for MyActor {
+//!     async fn a_method(&mut self, ...) -> ... {
+//!         todo!()
+//!     }
+//! }
+//! ```
+//!
+//! While inherent `impl SomeState` blocks are allowed within the module, there is currently no support for calling these from outside the actor - all externally callable methods must be defined on a performance. (See `canonical` performances later for a simplification of the common case)
+//!
+//! For methods inside a performance block, `Self` refers to the state type. (e.g. `SomeState` above) The above is the "strongest" form of signature - method implementations that do not `await` anything or mutate the state object do not have to include the respective keywords.
+//!
+//! **N.B.** While an actor's message handler can `await` futures (whether from an [`Envelope`] or otherwise) the event loop cannot resume until the method returns. This risks deadlocks where two actors end up awaiting replies from each other. If you need to handle the return value from calling another actor without blocking the original sender by waiting on it, consider using [`send_future_to`] or [`send_reply_to`] and passing the sender's handle.
+//!
+//! A performance implementation is allowed to be outside of the actor `mod` scope, in the same way that any other `impl ... for` block can be anywhere within the crate, but if it is elsewhere, the `mod` must contain a `#[performance] impl ARole for MyActor {}` block, including empty braces.
+//!
+//! Defining a role is syntatically a normal trait declaration with the appropriate macro attached:
+//!
+//! ```ignore
+//! #[role]
+//! trait ARole {
+//!     fn a_method(&mut self, ...) -> ...;
+//! }
+//! ```
+//!
+//! The methods on this trait should *not* be marked `async`, that is handled by the macro. Additionally, there are a number of restrictions on the trait's methods - see the [the macro documentation](shakespeare_macro::role) for the specifics.
+//!
+//! It's expected that many traits have a single "primary" implementation, such as an application object having a particular interface that testing mock implementations conform to. To simplify this situation, instead a role can be defined implicitly via a performance by providing:
+//! ```ignore
+//! #[performance(canonical)]
+//! impl ARole for MyActor {
+//!     ... // the trait ARole will be defined as part of generating the performance
+//! }
+//! ```
+//!
+//! ### Miscellenia
+//!
+//! A method inside a performance can define its *second* parameter (i.e. the one immediately after the `self`) as having a type of `&'_ mut Context<Self>` to get access to the [`Context`] object for the current actor, which includes the capability of getting the current actor's handle or shutting it down early.  The context parameter should *not* be included in any explicitly defined roles, and roles defined by `canonical` performances take this into account.
+//!
+//! There are several events in the actor's lifecycle that are accessed by optionally defining freestanding (i.e. outside of any `impl`) functions within the `actor` module. Their names, inputs and events are:
+//!
+//! * `stop(self)` - is called with the final value of the actor's state object when the actor shuts down without panicking
+//!	* `catch(self, Box<dyn Any + Send>)` - called in the event a method handler panics, being provided the final state value and the value passed to the `panic!()` call
+//!
+//! Both of these functions can have any `'static + Sized` return type, and any return values from these functions will be passed back to the [`ActorHandle`].
+//!
+//! **N.B.**: The `catch` function is not technically running in an unwinding context, so a secondary panic will not abort the process. However, Shakespeare leaves behaviour in this case unspecified except that safety is upheld, and **the exact behaviour may change without warning**.
+//!
+//! ## Actor Lifetime
+//!
+//! ### Start
+//!
+//! Calling `Actor::start(state)` spawns a new task for handling the actor's event loop. It is expected that you do any setup needed for the actor to be in a ready state in the construction of the `state` value itself, and there is currently no interface for running user code after the actor is constructed but before the event loop proper begins. However, while the event loop has technically started by the time that `start` returns, the only way to provide messages to process is via the handle coming out of `start` - there is no global broadcasting that might pre-empt this. This means that if a "guaranteed first call" is needed, this can be achieved by simply sending a message and waiting for a response before sharing the handle.
+//!
+//! Once the event loop is established, it awaits a message indicating a call made against the shell, via any of the roles the actor might have, and then calls the appropriate method from the corresponding `performance` for any it receives.
+//!
+//! ### Synchronisation
+//!
+//! The order the actor responds to calls from different tasks is unspecified. The order the actor responds to calls made via two different roles is unspecified *even from the same task or from the same handle.* A call will *happen-before* another call if the second call is made via a method defined by the same role, and from the same task, as the first call. Calls made by the actor's own performances count as being made on the same task as each other.
+//!
+//!
+//! ### Shutting down
+//!
+//! The actor can stop processing messages and shut down in several circumstances:
+//!
+//! 1. If a message handler panics, `catch` is called (or the panic value passed straight up to the [`ActorHandle`] if there is no `catch`) immediately. No further messages are processed, and attempting to send messages to the actor will fail by returning `Err` to the caller.
+//! 2. If the [`Context::stop`] is called, no further messages are processed, calls against the actor will return `Err`, but the actor's `stop` function is called rather than `catch`. This similarly passes the returned value up to the [`ActorHandle`].
+//! 3. If the `Arc` that was returned from `start` and all of its copies drop, *and* no further messages are waiting to be processed, `stop` will be called as in case 2. By definition, it is not possible for an external client to be sending messages to the actor at this point. (Note that functions directly subscribing the actor to a future result, such as [`send_stream_to`] implicitly hold an `Arc` and will preclude this case until that value yields.)
+//!
+//! **N.B:** Because method implementations can get hold of the actor's own handle via the [`Context`], then even if all other copies have dropped at any given time, a running event handler can "save" the actor by sending a new copy of the handle out of the actor. This is not treated as the actor being revived from having shut down, but instead it has not shut down in the first place.
+//!
+//! As an implementation detail of making all of the above work, *every actor* has a watchdog timer that fires intermittently to check for case 3 above, *whether or not* handles to the actor remain live. As a result, there is both a marginal amount of CPU use even by idle actors, and also a finite "finalization" interval between processing stopping (i.e. the later of the last handle dropping and the last message handler completing) and the actor beginning to shut down by calling `stop`. The exact length of this interval **is deliberately left unspecified**, and the behaviour may vary in future versions. Currently, this timer goes off 1 second (1000ms) after the last message was received, and recurs at the same rate if the actor is still alive at that point. This is considered a design issue and may be removed entirely in future versions.
+#![forbid(unsafe_code)]
+#![forbid(future_incompatible)]
 #![warn(missing_copy_implementations)]
 #![warn(missing_debug_implementations)]
 #![warn(missing_docs)]
@@ -11,7 +136,7 @@
 #![warn(clippy::dbg_macro)]
 #![warn(clippy::todo)]
 #![warn(clippy::unimplemented)]
-#![forbid(unsafe_code)]
+#![allow(clippy::tabs_in_doc_comments)]
 
 use std::any::Any;
 use std::future::Future;
@@ -30,12 +155,12 @@ mod core;
 mod tokio;
 
 pub use core::{
-	Accepts, Emits, Envelope, Handle as ActorHandle, Outcome as ActorOutcome, Role,
+	Accepts, Context, Emits, Envelope, Handle as ActorHandle, Outcome as ActorOutcome, Role,
 	Shell as ActorShell, Spawn as ActorSpawn, State as ActorState,
 };
 #[doc(hidden)]
 pub use core::{
-	Channel, Context, Receiver as RoleReceiver, ReturnCaster, ReturnEnvelope, ReturnPath,
+	Channel, Receiver as RoleReceiver, ReturnCaster, ReturnEnvelope, ReturnPath,
 	Sender as RoleSender,
 };
 
