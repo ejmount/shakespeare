27ca866403f9c75e2d5930acb6637cd3308b0623
Ewan Mount <ejmount@users.noreply.github.com>
2025-12-30 20:43:40 +0000

    Renaming StreamMessage method to feed_to

--- src/sendable.rs
+++ src/sendable.rs
@@ -12,7 +12,7 @@ pub trait Message: Future + Send + 'static {
 	/// The future's output will be delivered to the actor's mailbox when it resolves.
 	/// See the [`Accepts`] documentation for the conditions that allow an actor to use this function.
 	///
-	/// See also [`MessageStream::send_to`] if you have a [`Stream`] of items to deliver rather than a single value.
+	/// See also [`MessageStream::feed_to`] if you have a [`Stream`] of items to deliver rather than a single value.
 	///
 	/// **N.B**: this function retains the `Arc<dyn Role>` for as long as the future is pending, and will keep the actor alive for that time.
 	fn send_to<R>(self, actor: Arc<R>)
@@ -36,12 +36,12 @@ impl<T> Message for T where T: Future + Send + 'static {}
 
 /// Extension utilities for [`Stream<T>`]. Blanket implemented for all values that meet the requirements.
 pub trait MessageStream: Stream<Item: Send> + Send + 'static {
-	/// Subscribes an actor to a [`Stream`], delivering each item of the stream to the actor's mailbox.
+	/// Subscribes an actor to a [`Stream`], delivering each item of the stream to the actor's mailbox from a new task.
 	///
 	/// See the [`Accepts`] documentation for the conditions that allow an actor to use this function.
 	///
 	/// This function does not do anything to inform the actor when the stream closes, successfully or otherwise. If sending the stream item to the actor fails, the stream will be dropped. If an actor explicitly shuts down with an active stream, the stream will be dropped with any remaining items unread. A sent stream prevents an actor shutting down from zero remaining handles until the stream runs out, and conversely, the stream running out will release the held handle.
-	fn send_to<R>(self, actor: Arc<R>)
+	fn feed_to<R>(self, actor: Arc<R>)
 	where
 		Self: Sized,
 		R: 'static + ?Sized + Accepts<Self::Item>,
